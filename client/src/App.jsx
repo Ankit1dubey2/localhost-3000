0ce3f6521a8216aac01716cd7a3bb6c90aeb9dac
prathamesh <prathameshsk94@gmail.com>
2024-02-18 00:21:16 +0530

    React project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return <h1 className=" bg-zinc-800 text-white">BeatMarket 🔥</h1>;
+}
+
+export default App;
---
+++ client/src/components/FeaturedArtists.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeaturedArtists = () => {
+  return <div>FeaturedArtists</div>;
+};
+
+export default FeaturedArtists;
