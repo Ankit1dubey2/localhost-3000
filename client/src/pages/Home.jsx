fde13bb41d898b0111a04626908aaca0dee5c6f5
prathamesh <prathameshsk94@gmail.com>
2024-02-18 10:39:22 +0530

    added header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className=" bg-zinc-800 w-screen h-screen">
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
