51b39444fb8044099f383e440188dcc22bb8441b
prathamesh <prathameshsk94@gmail.com>
2024-02-18 11:50:49 +0530

    added carousel, created trending and popular section

--- client/src/components/Popular.jsx
+++ client/src/components/Popular.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import Carousel from "./Carousel";
+
+const data = [
+  {
+    img: "https://illpeoplemusic-v2-asserts-dev.s3.ap-south-1.amazonaws.com/images/genres/drill.jpg",
+    title: "K$SNA",
+  },
+  {
+    img: "https://illpeoplemusic-v2-asserts-dev.s3.ap-south-1.amazonaws.com/images/genres/drill.jpg",
+    title: "K$SNA",
+  },
+  {
+    img: "https://illpeoplemusic-v2-asserts-dev.s3.ap-south-1.amazonaws.com/images/genres/drill.jpg",
+    title: "K$SNA",
+  },
+  {
+    img: "https://illpeoplemusic-v2-asserts-dev.s3.ap-south-1.amazonaws.com/images/genres/drill.jpg",
+    title: "K$SNA",
+  },
+  {
+    img: "https://illpeoplemusic-v2-asserts-dev.s3.ap-south-1.amazonaws.com/images/genres/drill.jpg",
+    title: "K$SNA",
+  },
+  {
+    img: "https://illpeoplemusic-v2-asserts-dev.s3.ap-south-1.amazonaws.com/images/genres/drill.jpg",
+    title: "K$SNA",
+  },
+  {
+    img: "https://illpeoplemusic-v2-asserts-dev.s3.ap-south-1.amazonaws.com/images/genres/drill.jpg",
+    title: "K$SNA",
+  },
+  {
+    img: "https://illpeoplemusic-v2-asserts-dev.s3.ap-south-1.amazonaws.com/images/genres/drill.jpg",
+    title: "K$SNA",
+  },
+  {
+    img: "https://illpeoplemusic-v2-asserts-dev.s3.ap-south-1.amazonaws.com/images/genres/drill.jpg",
+    title: "K$SNA",
+  },
+];
 
 const Popular = () => {
-  return <div>Popular</div>;
+  return (
+    <div className=" w-screen px-[6vw]">
+      <h2 className=" text-4xl font-medium">Popular Producers</h2>
+      <Carousel data={data} square={true} />
+    </div>
+  );
 };
 
 export default Popular;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import Trending from "../components/Trending";
+import Popular from "../components/Popular";
 
 const Home = () => {
   return (
-    <div className=" bg-zinc-800 w-screen h-screen">
+    <div className=" bg-zinc-800 w-screen h-screen text-white overflow-x-hidden flex flex-col gap-10">
       <Header />
+      <Trending />
+      <Popular />
     </div>
   );
 };
